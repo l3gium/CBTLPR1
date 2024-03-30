7e5e38f700f9f221d2063efb2575af4174557d55
Miguel Luizatto Alves <131935219+l3gium@users.noreply.github.com>
2024-03-30 16:30:09 -0300

    Update TP02Ex05.java

--- Trabalho Prático 02/TP02Ex05.java
+++ Trabalho Prático 02/TP02Ex05.java
@@ -1,1 +1,39 @@
 
+import java.util.Scanner;
+
+public class TP02Ex05
+{
+    /*
+     5. Armazenar seis valores em uma matriz de ordem 3x2. Apresentar os valores na tela.
+     */
+
+     //Desenvolvido por Beatriz Bastos Borges e Miguel Luizatto Alves
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+
+        int[][] array = new int[3][2];
+
+        for(int i = 0; i  < 3; i++)
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                System.out.print("Digite o valor na posicao " + (i + 1) + ", " +(j + 1) + ": ");
+                array[i][j] = scan.nextInt();
+            }
+            System.out.println("\n");
+        }
+
+        for(int i = 0; i  < 3; i++)
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                System.out.print(array[i][j]);
+                System.out.print("|");
+            }
+            System.out.print("\n");
+            System.out.print("----");
+            System.out.print("\n");
+        }
+    }
+}
